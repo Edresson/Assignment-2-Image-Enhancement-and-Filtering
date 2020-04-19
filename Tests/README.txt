4399e59cdd96682e706bf2ea4dd405115d1be8dc
Edresson <edresson1@gmail.com>
2020-04-19 12:11:38 -0300

    fix bilateral filter problem

--- Tests/README.txt
+++ Tests/README.txt
@@ -1,3 +1,3 @@
-Arquivo zip gerado em: 17/04/2020 14:43:12 
+Arquivo zip gerado em: 18/04/2020 17:43:41 
 Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
 Exercício: Assignment 2 : Image Enhancement and Filtering
--- submission/dip02_submission.py
+++ submission/dip02_submission.py
@@ -19,6 +19,7 @@ Assignment 2:  Image Enhancement and Filtering
 import imageio # improt imageio library
 import numpy as np # import numpy library
 
+
 def RSE(imgA,imgB): 
     """This function calculate Root Squared Error between Image A and Image B
     Parameters:
@@ -28,8 +29,20 @@ def RSE(imgA,imgB):
     # np.subtract calcute the error between pixels, after np.square square the error, after np.sum sum all error and finnaly np.sqrt calculate the root square
     return np.sqrt(np.sum(np.square(np.subtract(imgA.astype("float"), imgB.astype("float"))))) 
 
+def unpad(img, pad_len):
+    '''This function unpad  2D  Array padded with same lenght in the all axes
+    Parameters:
+        img: 2D source image
+        pad_len: padding factor used for padding Array
+    '''
+    return img[pad_len:-pad_len,pad_len:-pad_len]
 
 def euclidian_distance(x,y):
+    ''' This functions calculate the euclidian distance between point x and point y
+    Parameters:
+        x: point x
+        y: point y
+    '''
     return np.sqrt(x**2 + y**2)
 
 def gaussian_kernel(fil, sig):
@@ -38,60 +51,61 @@ def gaussian_kernel(fil, sig):
         fil: is the filter/image
         sig: is the sigma
     '''
-    return (1.0 / ( 2 *np.pi * (sig ** 2))) * np.exp(- (np.square(fil) / (2 * sig ** 2)))
+    return (1.0 / ( 2 *np.pi * (sig ** 2))) * np.exp(- (np.square(fil) / (2 *  (sig ** 2))))
 
 def bilateral_filter(input_img, n, sigma_s, sigma_r):
     '''This Function implement the bilateral filter. The Bilateral Filter 
         is a nonlinear filtering technique to smooth images while 
-        preservingedges
+        preserving edges
         
     '''
-    img_shape = input_img.shape
-    
+    pad_len = n-(int(n/2)+1)
+    # padding image
+    input_img = np.pad(input_img, pad_len, mode='constant')
+
+    #copy input image to output image
+    output_img = input_img.copy()
+
     # make a centred origin filter
     w = np.zeros((n,n))
+    img_shape = input_img.shape
+    filter_shape = w.shape
     # get points and aplied euclidian distance
     for i in range(n): 
         for j in range(n):
             x = int(-(n-1)/2)+i
             y = int(-(n-1)/2)+j
-            #print(x,y)
             w[i][j] = euclidian_distance(x,y)  
 
-    #print(w)
     # get filter with kernel gaussian transformation
     w = gaussian_kernel(w, sigma_s).astype(np.float)
-    #print(w)
 
-
-    filter_shape = w.shape
-    output_img = np.zeros(img_shape).astype(np.uint8)
-   
     x = int((filter_shape[0]-1)/2)
-    y = int((filter_shape[1]-1)/2)
-
+    y = int((filter_shape[1]-1)/2)  
 
     # Apply the convolution
     for i in range(x,img_shape[0]-x):
         for j in range(y,img_shape[1]-y):
-            '''Wp = np.zeros((n,n))
-            I_f = np.zeros((n,n)) ''' 
             Wp = 0 
-            I_f = 0     
+            I_f = 0
             # step 1: compute the value of the range Gaussian component, using the Gaussian kernel equation, with σ=σr and x as the diference between the intensity ofthe neighbor pixel and the intensity of the center pixel
             neighbors = input_img[i-x : i+(x+1) , j-y : j+(y+1)].astype(np.float)
-            g_neighbors = gaussian_kernel(neighbors-input_img[i][j],sigma_r).astype(np.float)
-            
-            # step 2: compute the total value of the filter, on this neighbor, by multiplying the value of the range component by the value of the spatial component gsi(in the corresponding position)
-            g_w = np.multiply(g_neighbors,w).astype(np.float)
-            # step 3
-            Wp = g_w
-            # step 4
-            I_f =  np.multiply(g_w,neighbors).astype(np.float)
+            for k in range(filter_shape[0]):
+                for l in range(filter_shape[1]):
+                    #print(input_img[i][j],neighbors[k][l])
+                    g_neighbor = gaussian_kernel(neighbors[k][l]-input_img[i][j],sigma_r).astype(np.float)
+                    # step 2: compute the total value of the filter, on this neighbor, by multiplying the value of the range component by the value of the spatial component gsi(in the corresponding position)
+                    g_w = (g_neighbor * w[k][l])
+                    # step 3
+                    Wp = Wp + g_w
+                    # step 4
+                    I_f = I_f + np.multiply(g_w,neighbors[k][l])
+            # end step        
+            I_f = I_f/Wp
+            output_img[i][j] = (I_f).astype(np.uint8)
 
-            output_img[i,j] = (np.sum(I_f)/np.sum(Wp)).astype(np.uint8)
-
-
+    #unpad image
+    output_img = unpad(output_img, pad_len)
     return output_img
 
 def unsharp_mask_laplacian(input_img, c, kernel):
@@ -111,7 +125,7 @@ def main():
         sigma_s = float(input())
         sigma_r = float(input())
         output_img = bilateral_filter(input_img, n, sigma_s, sigma_r)
-
+        
     if method == 2: # if method 2
         c = float(input()) # its float c <= 1
         kernel = int(input()) # its 1 or 2
